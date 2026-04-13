00c024b94a665a4be53f26b07281315221e1ddf6
Nico-Chacon <nicochacon531@gmail.com>
2026-04-13 20:50:52 +0000

    feat: Subir proyecto

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hola desde mi API académica!');
+});
+
+server.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
